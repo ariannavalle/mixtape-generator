c09d660f5a58bbb005977d8abeac461df5fce07b
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-10-14 11:33:58 -0400

    Add styles for landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,9 +5,17 @@ import './LandingPage.css'
 export default function LandingPage() {
     return (
         <div>
-            <h1>The ultimate mixtape generator</h1>
-            <h2>Discover new music and create curated playlists.</h2>
-            <Link to={'/generator'}>Go</Link>
+            <h1>The ultimate
+                <br />mixtape generator</h1>
+            <h2>Discover new music and create curated playlists.<br />
+                <Link to={'/generator'}>
+                    <div class="box-2">
+                        <div class="btn btn">
+                            <span>START</span>
+                        </div>
+                    </div>
+                </Link>
+            </h2>
         </div>
     )
 }
