5dea76eb1166d3f5fbf279978c84b5306b2b7d06
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-10-19 21:03:35 -0400

    Update Tracklist.js

--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -2,19 +2,57 @@ import React, { Component } from 'react'
 import './Tracklist.css'
 
 export default class Tracklist extends Component {
+    state = {
+        currentlyPlaying: false
+    }
+
+    playAudio = (e) => {
+        if (e.target !== e.currentTarget) {
+            console.log(e.target, e.target.previousElementSibling)
+
+            if (this.state.currentlyPlaying) {
+                e.target.src = "https://images.vexels.com/media/users/3/135547/isolated/preview/cb0c341c8b97e34abb0d20543a14eab4-button-play-icon-by-vexels.png"
+                e.target.previousElementSibling.pause()
+
+            } else {
+                e.target.src = "https://images.vexels.com/media/users/3/135545/isolated/preview/d744ce91510415a8e3de69cd616b3fae-button-pause-icon-by-vexels.png"
+                e.target.previousElementSibling.play();
+            }
+            this.setState({
+                currentlyPlaying: !this.state.currentlyPlaying
+            })
+            e.stopPropagation()
+        }
+    }
+
     render() {
         const { recommendations } = this.props
         return (
-            <div>
-                <div className="tracklist-container">
-                    {recommendations.map((rec, id) => {
-                        return <div key={rec.id}>{id + 1}.{rec.name} - {rec.artists[0].name}
-                            <audio controls src={rec.preview_url}>Your browser does not support the <code>audio</code> element.</audio>
+
+            <div id="tracklist-container" onClick={(e) => this.playAudio(e)} >
+                {recommendations.map((rec, i) => {
+                    return <div key={rec.id} className="track">
+                        <div className="album-art">
+                            <img src={rec.album.images[0].url} className="cover" />
                         </div>
-                    })
-                    }
-                </div>
+                        <div className="track-info">
+                            <div className="track-title">{rec.name}</div>
+                            <div className="track-artist">by {rec.artists[0].name}</div>
+                        </div>
+
+                        <audio id="my-audio">
+                            <source src={rec.preview_url} type="audio/mpeg" />
+                                        Your browser does not support the audio element.
+                                    </audio>
+
+                        {rec.preview_url &&
+                            <img src="https://images.vexels.com/media/users/3/135547/isolated/preview/cb0c341c8b97e34abb0d20543a14eab4-button-play-icon-by-vexels.png" className="audio-icons" id={i} />
+                        }
+                    </div>
+                })
+                }
             </div>
+
         )
     }
 }
