e01067f06e29645848c8b2aad5abf5ce2126bb54
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-10-20 16:14:26 -0400

    Added animated text

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import './LandingPage.css'
 
-export default function LandingPage() {
-    return (
-        <div className="home-body">
-            <h1>The ultimate
-                <br />mixtape generator</h1>
-            <h2>Discover new music and create curated playlists.<br />
-                <Link to={'/generator'}>
-                    <div className="box-2">
-                        <div className="btn btn">
-                            <span>START</span>
+export default class LandingPage extends Component {
+
+    componentDidMount = () => {
+        const string = "The ultimate mixtape generator";
+        const str = string.split("");
+        const el = document.getElementById('str');
+        (function animate() {
+            const running = setTimeout(animate, 90);
+            str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
+        })();
+
+    }
+    render() {
+        return (
+            <div className="home-body">
+                <h1 id="str"></h1>
+                <h2>Discover new music and create curated playlists.<br />
+                    <Link to={'/generator'}>
+                        <div className="box-2">
+                            <div className="btn btn">
+                                <span>START</span>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            </h2>
-        </div>
-    )
+                    </Link>
+                </h2>
+            </div>
+        )
+    }
 }
+
+
