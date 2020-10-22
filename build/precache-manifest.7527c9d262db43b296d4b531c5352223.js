3b641c7f832c5f106322c8b1eb85a573029d68de
ariannavalle <61655861+ariannavalle@users.noreply.github.com>
2020-10-22 16:08:12 -0400

    Add gh-pages config

---
+++ build/precache-manifest.7527c9d262db43b296d4b531c5352223.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9802c0fc803dd2ef50c5fabd6a01d7",
+    "url": "/mixtape-generator/index.html"
+  },
+  {
+    "revision": "202cccbbf643807ead92",
+    "url": "/mixtape-generator/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "62d5a143832f774c60b0",
+    "url": "/mixtape-generator/static/css/main.4b721963.chunk.css"
+  },
+  {
+    "revision": "202cccbbf643807ead92",
+    "url": "/mixtape-generator/static/js/2.42ae2e93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mixtape-generator/static/js/2.42ae2e93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d5a143832f774c60b0",
+    "url": "/mixtape-generator/static/js/main.bccc55e9.chunk.js"
+  },
+  {
+    "revision": "700a5256ff2c3ae3dddc",
+    "url": "/mixtape-generator/static/js/runtime-main.45e82cdb.js"
+  }
+]);
